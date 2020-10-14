e533b5e26a695a65759cdd2bdc735d15801b6f09
ArbiNMaki <arbi.nmaki919@gmail.com>
2020-10-15 03:23:47 +0700

    Convert HTML to Svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/7907/46836605454_b666ae1b32_h.jpg',
+            pledged: 9000,
+            target: 10000,
+            date_end: +new Date('15 November 2020'),
+            profile_picture: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
